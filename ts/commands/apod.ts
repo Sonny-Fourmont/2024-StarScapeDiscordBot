03771003cceac8f3158c710a3d2c89ce5115ad2b
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-03-29 18:56:33 +0100

    [ADD] Opti apod command (do not reprocessing if the image already exists)... [FIX] switch date format (2000-01-01 -> 01/01/2000)... [FIX] split the main function (apod) to another functions for each features... [ADD] more log in the host's terminal

--- ts/commands/apod.ts
+++ ts/commands/apod.ts
@@ -1,27 +1,68 @@
 import { formattedDate } from "../app";
 import { exec } from "child_process";
+import fs from "fs";
+
+async function checkExist(interaction: any, path: string) {
+    if (fs.existsSync(`${path}`)) {
+        console.log(`${path}: already exists`);
+        await interaction.editReply({files: [`${path}`]});
+        console.log('Image sent')
+        return true;
+    } else {
+        console.log(`${path}: doesn't exists`);
+        return false;
+    }
+}
+
+async function daily(interaction: any) {
+    if (!await checkExist(interaction, `./assets/images/APOD/iod-${formattedDate}.jpg`)) {
+        console.log("Image processing...");
+        exec(`python3 ./assets/script/apod-maker.py ${formattedDate}`, async (error, stdout, stderr) => {
+            console.log(stdout);
+            if (!(stdout == "Image processing complete.\n")) {
+                await interaction.editReply("Astronomy Picture Of The Day is not available yet");
+                console.log("Astronomy Picture Of The Day is not available yet");
+            } else {
+                await interaction.editReply({files: [`./assets/images/APOD/iod-${formattedDate}.jpg`]});
+                console.log('Image sent');
+            }
+        });
+    }
+}
+
+async function custom(interaction: any, arg: string) {
+    if (!await checkExist(interaction,  `./assets/images/APOD/iod-${arg}.jpg`)) {
+        console.log("Image processing...");
+        exec(`python3 ./assets/script/apod-maker.py ${arg}`, async (error, stdout, stderr) => {
+            console.log(stdout);
+            if (!(stdout == "Image processing complete.\n")) {
+                await interaction.editReply(`Astronomy Picture Of The Day for the "${arg}" doesn't exists or is not available`);
+                console.log(`Astronomy Picture Of The Day for the "${arg}" doesn't exists or is not available`);
+            } else {
+                await interaction.editReply({files: [`./assets/images/APOD/iod-${arg}.jpg`]});
+                console.log('Image sent');
+            }
+        });
+    }
+}
+
+async function parseArgs(args: string) {
+    const splittedArgs: string[] = args.split("/", 3);
+    const parsedArgs: string = `${splittedArgs[2]}-${splittedArgs[1]}-${splittedArgs[0]}`;
+    console.log(parsedArgs)
+    return parsedArgs;
+}
 
 async function apod(interaction: any) {
     if (interaction.commandName === "apod") {
         if (!interaction.options.get('date')) {
             await interaction.deferReply();
-            exec(`python3 ./assets/script/apod-maker.py ${formattedDate}`, async (error, stdout, stderr) => {
-                console.log(stdout);
-                if (!(stdout == "Image processing complete.\n"))
-                    await interaction.editReply("Astronomy Picture Of The Day is not available yet");
-                else
-                    await interaction.editReply({files: [`./assets/images/APOD/iod-${formattedDate}.jpg`]});
-            });
+            daily(interaction);
         } else {
-            const arg = interaction.options.get('date').value;
+            const arg: string = interaction.options.get('date').value;
+            const parsedArgs: string = await parseArgs(arg);
             await interaction.deferReply();
-            exec(`python3 ./assets/script/apod-maker.py ${arg}`, async (error, stdout, stderr) => {
-                console.log(stdout);
-                if (!(stdout == "Image processing complete.\n"))
-                    await interaction.editReply(`Astronomy Picture Of The Day for the "${arg}" doesn't exist or is not available`);
-                else
-                    await interaction.editReply({files: [`./assets/images/APOD/iod-${arg}.jpg`]});
-            });
+            custom(interaction, parsedArgs);
         }
     }
 }
